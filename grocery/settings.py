0ec6b2f6669d465eb357f476393d168a83650300
mdzuelrana05@gmail.com <mdzuelrana05@gmail.com>
2026-03-05 23:40:10 +0600

    cors

--- grocery/settings.py
+++ grocery/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cart',
     'review',
     'payments',
+    "corsheaders",
     'rest_framework',
     'djoser',
     
@@ -56,7 +57,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -144,7 +146,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATICFILES_STORAGE="whitenoise.storage.CompressedStaticFilesStorage"

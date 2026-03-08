e14ef6d893b57fedbba2b1be910ce9b76a29a69e
mdzuelrana05@gmail.com <mdzuelrana05@gmail.com>
2026-03-08 21:28:36 +0600

    err

--- cart/serializers.py
+++ cart/serializers.py
@@ -46,16 +46,33 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 
 
+
+
+
 class WishlistSerializer(serializers.ModelSerializer):
+
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    product_price = serializers.DecimalField(
+        source="product.price",
+        max_digits=10,
+        decimal_places=2,
+        read_only=True
+    )
+    #product_image = serializers.ImageField(source="product.image", read_only=True)
 
     class Meta:
         model = Wishlist
-        fields = ['id', 'product']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "product",
+            "product_name",
+            "product_price"
+            # "product_image"
+        ]
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        product = validated_data['product']
+        user = self.context["request"].user
+        product = validated_data["product"]
 
         wishlist_item, created = Wishlist.objects.get_or_create(
             user=user,

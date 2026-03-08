eec870f6e6399b28dd294493fdb8b7dcf9f27c44
mdzuelrana05@gmail.com <mdzuelrana05@gmail.com>
2026-03-08 19:47:08 +0600

    cart

--- cart/models.py
+++ cart/models.py
@@ -16,4 +16,6 @@ class CartItem(models.Model):
 class Wishlist(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ['user', 'product']
 
--- cart/serializers.py
+++ cart/serializers.py
@@ -1,12 +1,54 @@
 from rest_framework import serializers
-from cart.models import CartItem, Wishlist
+from cart.models import CartItem, Wishlist,Cart
+from tasks.models import Product
+
 
 class CartItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CartItem
-        fields = '__all__'
+        fields = ['id', 'product', 'quantity']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+
+        # get or create user's cart
+        cart, created = Cart.objects.get_or_create(user=user)
+
+        product = validated_data['product']
+        quantity = validated_data.get('quantity', 1)
+
+        # if product already in cart, increase quantity
+        cart_item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            product=product,
+            defaults={'quantity': quantity}
+        )
+
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
+
+        return cart_item
+
+
+
 
 class WishlistSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Wishlist
-        fields = '__all__'
+        fields = ['id', 'product']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        product = validated_data['product']
+
+        wishlist_item, created = Wishlist.objects.get_or_create(
+            user=user,
+            product=product
+        )
+
+        return wishlist_item

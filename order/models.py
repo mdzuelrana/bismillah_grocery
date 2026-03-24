9e744a62fc3ad4d46ee0686f9503192d2065d66c
mdzuelrana05@gmail.com <mdzuelrana05@gmail.com>
2026-03-24 19:20:05 +0600

    migrate order model

--- order/models.py
+++ order/models.py
@@ -19,9 +19,9 @@ class Order(models.Model):
     customer = models.ForeignKey(User,on_delete=models.CASCADE)
 
     total_amount = models.DecimalField(max_digits=10,decimal_places=2)
-    full_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
-    address = models.TextField()
+    full_name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
     payment_status = models.CharField(
         max_length=20,
         choices=PAYMENT_STATUS,

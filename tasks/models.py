5957b3c3fadb95650fcdc3669f14de1a02330271
mdzuelrana05@gmail.com <mdzuelrana05@gmail.com>
2026-04-17 12:03:59 +0600

    image

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class Category(models.Model):
@@ -10,14 +11,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    
     seller   = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name        = models.CharField(max_length=200)
     description = models.TextField()
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     stock       = models.PositiveIntegerField()
-    image       = models.ImageField(upload_to='products/', null=True, blank=True)  # ✅ added
+    image       = models.ImageField(
+        upload_to='products/',
+        storage=S3Boto3Storage(),  # ✅ forces Supabase S3
+        null=True,
+        blank=True
+    )
     created_at  = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
